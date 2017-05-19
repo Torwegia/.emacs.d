e34d76980973f282bfdd2f4f31b5bbc3056dfc2a
Tor Loken <tor@uber.com>
2017-05-19 13:25:46 -0700

    Move customizations to thier own file
    This prevents the init.el from getting full of junk

--- init.el
+++ init.el
@@ -6,6 +6,11 @@
 ;; No splash
 (setq inhibit-startup-message t)
 
+;; Move custom junk
+(setq custom-file
+      (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file 'noerror)
+
 ;; Set dependencies
 (setq my-stuff-dir
      (expand-file-name "my-stuff" user-emacs-directory))
@@ -67,18 +72,3 @@
 
 (dolist (my-pkg my-packages)
     (require my-pkg))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (evil rainbow-mode rainbow-delimiters monokai-theme diminish color-identifiers-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
